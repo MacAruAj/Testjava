fd50cbad8044bd4de66dc5aa4258af66a904d4d3
MacAruAj <126191806+MacAruAj@users.noreply.github.com>
2023-05-21 21:14:10 +0530

    Update Model.java

--- src/main/java/com/microservice/ratingservicee/RatingServicee/Entity/Model.java
+++ src/main/java/com/microservice/ratingservicee/RatingServicee/Entity/Model.java
@@ -2,7 +2,7 @@ package com.microservice.ratingservicee.RatingServicee.Entity;
 
 public class Model {
   void so(){
-  
+   System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiii");
   
   System.out.println("hellooooooooooooooooooooooooooooooooooooo");
   }
